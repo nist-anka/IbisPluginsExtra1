948101efe04f5dfd169d74354425f88e1c59eac1
anka_at_ipl <pluszcz@gmail.com>
2019-02-21 11:41:18 -0500

    Finished working interface to set object's transform programmatically.

--- ApplyTransformToObject/applytransformtoobjectplugininterface.cpp
+++ ApplyTransformToObject/applytransformtoobjectplugininterface.cpp
@@ -4,13 +4,6 @@
 #include <QString>
 #include "applytransformtoobjectwidget.h"
 
-
-#include "vtkTransform.h"
-#include "vtkMatrix4x4.h"
-#include "vtkSmartPointer.h"
-#include "ibisapi.h"
-#include "sceneobject.h"
-
 ApplyTransformToObjectPluginInterface::ApplyTransformToObjectPluginInterface()
 {
 }
@@ -21,17 +14,10 @@ ApplyTransformToObjectPluginInterface::~ApplyTransformToObjectPluginInterface()
 
 QWidget * ApplyTransformToObjectPluginInterface::CreateFloatingWidget()
 {
-    // Start the window
+    // Open the widget
     m_interfaceWidget = new ApplyTransformToObjectWidget( 0 );
     m_interfaceWidget->setAttribute( Qt::WA_DeleteOnClose, true );
     m_interfaceWidget->setWindowTitle( "Apply Transform To Object" );
-
+    m_interfaceWidget->SetInterface( this );
     return m_interfaceWidget;
-    /*
-    vtkMatrix4x4 *outMatrix = ref->GetLocalTransform()->GetMatrix();
-    std::cout << "Output matrix:\n";
-    std::cout << outMatrix->GetElement( 0, 0 ) << " " << outMatrix->GetElement( 0, 1 ) << "." << outMatrix->GetElement( 0, 2 ) << "." << outMatrix->GetElement( 0, 3 ) << std::endl;
-    std::cout << outMatrix->GetElement( 1, 0 ) << " " << outMatrix->GetElement( 1, 1 ) << "." << outMatrix->GetElement( 1, 2 ) << "." << outMatrix->GetElement( 1, 3 ) << std::endl;
-    std::cout << outMatrix->GetElement( 2, 0 ) << " " << outMatrix->GetElement( 2, 1 ) << "." << outMatrix->GetElement( 2, 2 ) << "." << outMatrix->GetElement( 2, 3 ) << std::endl;
-    */
 }
--- ApplyTransformToObject/applytransformtoobjectwidget.h
+++ ApplyTransformToObject/applytransformtoobjectwidget.h
@@ -2,10 +2,15 @@
 #define APPLYTRANSFORMTOOBJECTWIDGET_H
 
 #include <QWidget>
+#include "applytransformtoobjectplugininterface.h"
+
+class SceneObject;
 
 namespace Ui {
 class ApplyTransformToObjectWidget;
 }
+
+class vtkQtMatrixDialog;
 
 class ApplyTransformToObjectWidget : public QWidget
 {
@@ -14,13 +19,21 @@ class ApplyTransformToObjectWidget : public QWidget
 public:
     explicit ApplyTransformToObjectWidget(QWidget *parent = 0);
     ~ApplyTransformToObjectWidget();
+    void SetInterface( ApplyTransformToObjectPluginInterface *intface );
 
 private slots:
 
     void on_transformPushButton_clicked();
-    void on_applyPushButtonClicked();
+    void on_sceneObjectsComboBox_currentIndexChanged(int index);
+    void EditMatrixDialogClosed();
+    void UpdateUI();
 
 private:
+    vtkQtMatrixDialog * m_matrixDialog;
+    SceneObject *m_selectedObject;
+
+    ApplyTransformToObjectPluginInterface *m_pluginInterface;
+
     Ui::ApplyTransformToObjectWidget *ui;
 };
 
